76020fdc5d02dc97566684ba18cd72ee377b9256
erianvc <erianvc.dev@gmail.com>
2020-08-20 22:57:56 -0500

    refactor: replace deprecated gap utilities

--- postcss.config.js
+++ postcss.config.js
@@ -7,12 +7,11 @@ const cssnano = require('cssnano')({
     }],
 })
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./src/**/*.html'],
+    content: ['./public/**/*.html'],
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 })
 
 module.exports = {
-
     plugins: [
         tailwindcss('./tailwind.config.js'),
         ...(process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []),
